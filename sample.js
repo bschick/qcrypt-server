90cfc4ba7bf2f4ae79ce8e91524f2b0144f20cd1
Anonymous <anonymous@kali>
2025-11-22 13:18:49 -0800

    chore: cleanup temporary files

--- sample.js
+++
@@ -1,3 +0,0 @@
-import { WebAuthnEmulator } from "nid-webauthn-emulator";
-const emu = new WebAuthnEmulator({ rpId: "t1.quickcrypt.org", origin: "https://t1.quickcrypt.org:4200" });
-console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(emu)));
